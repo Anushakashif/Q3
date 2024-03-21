5f1d3e996ba1381bb5bd008c3d45d3d203ee1608
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 14:24:25 -0700

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+// 2. Name Cases: Store a persons name in a variable, and then print that persons name in lowercase, uppercase and titlecase.
+let firstName = "anusha kashif";
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
---
+++ Q3.ts
@@ -0,0 +1,7 @@
+// 2. Name Cases: Store a persons name in a variable, and then print that persons name in lowercase, uppercase and titlecase.
+
+let firstName: string = "anusha kashif";
+
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
